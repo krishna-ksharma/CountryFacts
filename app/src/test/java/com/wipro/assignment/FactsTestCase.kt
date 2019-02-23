e8f547eaf419f40b8379756c120c4161f82bf756
Krishna Sharma <krishna.ksharma84@gmail.com>
2019-02-24 00:33:35 +0530

    Fix for Robolectric manifest missing

--- app/src/test/java/com/wipro/assignment/FactsTestCase.kt
+++ app/src/test/java/com/wipro/assignment/FactsTestCase.kt
@@ -9,7 +9,7 @@ import org.robolectric.annotation.Config
  * Created by krishnas on 2/23/2019.
  */
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Build.VERSION_CODES.P], application = TestFactsApplication::class)
+@Config(manifest = "app/src/main/AndroidManifest.xml", sdk = [Build.VERSION_CODES.P], application = TestFactsApplication::class)
 abstract class FactsTestCase {
 
 }
